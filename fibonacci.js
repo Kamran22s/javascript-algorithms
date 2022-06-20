47df07fb5a5a902762455520b1029178e9af730a
Hoang An <an.tonnguyenhoang@gmail.com>
2022-06-20 18:53:45 +0700

    cm

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+const fibonacci = (n) => {
+	const fib = [0, 1];
+	for (let i = 2; i < n; i++) {
+		fib[i] = fib[i - 2] + fib[i - 1];
+	}
+	return fib;
+}
+
+console.log(fibonacci(2)); // [0, 1]
+console.log(fibonacci(3)); // [0, 1, 1]
+console.log(fibonacci(7)); // [0, 1, 1, 2, 3, 5, 8]
+
+// Big-O = O(n)
